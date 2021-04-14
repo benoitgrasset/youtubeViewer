125611dbbea7fe004c74ede081e580ddd6f977ab
benoitgrasset <benoit.grasset@gmail.com>
2021-04-14 10:57:56 +0200

    Updates

---
+++ precache-manifest.614eb4eac8fa4936a5262f521aac63a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7855d885589fed6fffe901e437a3a2",
+    "url": "/youtube-viewer/index.html"
+  },
+  {
+    "revision": "423b752b98e534977ebe",
+    "url": "/youtube-viewer/static/css/main.9b905e78.chunk.css"
+  },
+  {
+    "revision": "f34c94c63d900b8cd01f",
+    "url": "/youtube-viewer/static/js/2.adc37937.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/youtube-viewer/static/js/2.adc37937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423b752b98e534977ebe",
+    "url": "/youtube-viewer/static/js/main.ac99b952.chunk.js"
+  },
+  {
+    "revision": "33814ce345cfa45eea20",
+    "url": "/youtube-viewer/static/js/runtime-main.fadc71aa.js"
+  },
+  {
+    "revision": "f95e2f430a19d8b3527b7cadc4bd6bc9",
+    "url": "/youtube-viewer/static/media/icone_yt.f95e2f43.png"
+  }
+]);
